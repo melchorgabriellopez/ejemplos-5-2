7e99e95edbe3c85ff2f57b36b62fe857d3374811
melchorgabriellopez <48580537+melchorgabriellopez@users.noreply.github.com>
2020-10-14 13:56:44 -0600

    materia

--- Proyecto clases/Clases/BaseDatos.cs
+++ Proyecto clases/Clases/BaseDatos.cs
@@ -1,24 +1,79 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Proyecto_clases.Clases
 {
 
     public static class BaseDatos
     {
-
-          static List<Alumno> TablaAlumnos=new List<Alumno>();
-
+        static List<Alumno> TablaAlumnos = new List<Alumno>();
+        static List<Materia> TablaMaterias = new List<Materia>();
 
         public static bool Guardar(Alumno alumno)
         {
             TablaAlumnos.Add(alumno);
             return true;
-        } 
+        }
+        public static bool Guardar(Materia materia)
+        {
+            TablaMaterias.Add(materia);
+            return true;
+        }
 
-        public static List<Alumno> Listar ()
+        public static void Actualizar(int id, Alumno alumno)
+        {
+            var actualizar = TablaAlumnos.Where(x => x.IdAlumno == id).FirstOrDefault();
+            actualizar.Nombre = alumno.Nombre;
+            actualizar.CURP = alumno.CURP;
+            actualizar.Activo = alumno.Activo;
+            actualizar.Sexo = alumno.Sexo;
+        }
+        public static void Actualizar(int id,Materia materia)
+        {
+           var actualizar= TablaMaterias.Where(x=> x.IdMateria==id).FirstOrDefault();
+           actualizar.Nombre=materia.Nombre;        
+        }
+
+        public static List<Alumno> ListarAlumno()
         {
             return TablaAlumnos;
         }
+        public static List<Materia> ListarMateria()
+        {
+            return TablaMaterias;
+        }
+
+
+        public static void BorrarListaAlumnos()
+        {
+            TablaAlumnos.Clear();
+            Console.WriteLine("Se borraron todos los alumnos...");
+            Console.ReadKey();
+        }
+
+
+        public static void BorrarListaMeterias()
+        {
+            TablaMaterias.Clear();
+            Console.WriteLine("Se borraron todas las materias...");
+            Console.ReadKey();
+        }
+
+        public static void BorrarAlumno(int id)
+        {
+            var borrar = TablaAlumnos.Where(x => x.IdAlumno == id).FirstOrDefault();
+            TablaAlumnos.Remove(borrar);
+        }
+
+        public static void BorrarMateria(int id)
+        {
+            var borrar = TablaMaterias.Where(x => x.IdMateria == id).FirstOrDefault();
+            TablaMaterias.Remove(borrar);
+        }
+
 
     }
+
+
 }
--- Proyecto clases/Program.cs
+++ Proyecto clases/Program.cs
@@ -1,53 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using Proyecto_clases.Clases;
-using Proyecto_clases.Enumeradores;
-
 namespace Proyecto_clases
 {
     class Program
     {
         static void Main(string[] args)
         {
-            /*  Alumno alumno= new Alumno();
-
-              alumno.IdAlumno=1;
-              alumno.Nombre="Pedro Perez";
-              alumno.Activo=true; 
-              alumno.FechaNacimiento=DateTime.Now;
-              alumno.Sexo=Sexo.Indefinido;
-              alumno.CURP="asdasdf";
-
-
-            List<Alumno> alumnos = new List<Alumno>();  
-
-            alumnos.Add(new Alumno{IdAlumno=1,Sexo=Sexo.Hombre,FechaNacimiento=DateTime.Parse( "25/01/2001"),CURP="asdasd",Activo=true,Nombre="Pedro perez" });   
-            alumnos.Add(new Alumno{IdAlumno=2,Sexo=Sexo.Mujer,FechaNacimiento=DateTime.Parse( "30/10/2001"),CURP="sadfsd",Activo=true,Nombre="Maria Soto" });   
-            alumnos.Add(new Alumno{IdAlumno=1,Sexo=Sexo.Hombre,FechaNacimiento=DateTime.Parse( "25/01/2001"),CURP="asdasd",Activo=true,Nombre="Pedro perez" });   
-            alumnos.Add(new Alumno{IdAlumno=2,Sexo=Sexo.Mujer,FechaNacimiento=DateTime.Parse( "30/10/2001"),CURP="sadfsd",Activo=true,Nombre="Maria Soto" });   
-            alumnos.Add(new Alumno{IdAlumno=1,Sexo=Sexo.Hombre,FechaNacimiento=DateTime.Parse( "25/01/2001"),CURP="asdasd",Activo=true,Nombre="Pedro perez" });   
-            alumnos.Add(new Alumno{IdAlumno=2,Sexo=Sexo.Mujer,FechaNacimiento=DateTime.Parse( "30/10/2001"),CURP="sadfsd",Activo=true,Nombre="Maria Soto" });        
-            alumnos.Add(new Alumno{IdAlumno=1,Sexo=Sexo.Hombre,FechaNacimiento=DateTime.Parse( "25/01/2001"),CURP="asdasd",Activo=true,Nombre="Pedro perez" });   
-            alumnos.Add(new Alumno{IdAlumno=2,Sexo=Sexo.Mujer,FechaNacimiento=DateTime.Parse( "30/10/2001"),CURP="sadfsd",Activo=true,Nombre="Maria Soto" });     
-            alumnos.Add(new Alumno{IdAlumno=1,Sexo=Sexo.Hombre,FechaNacimiento=DateTime.Parse( "25/01/2001"),CURP="asdasd",Activo=true,Nombre="Pedro perez" });   
-            alumnos.Add(new Alumno{IdAlumno=2,Sexo=Sexo.Mujer,FechaNacimiento=DateTime.Parse( "30/10/2001"),CURP="sadfsd",Activo=true,Nombre="Maria Soto" });   
-            alumnos.Add(new Alumno{IdAlumno=1,Sexo=Sexo.Hombre,FechaNacimiento=DateTime.Parse( "25/01/2001"),CURP="asdasd",Activo=true,Nombre="Pedro perez" });   
-            alumnos.Add(new Alumno{IdAlumno=2,Sexo=Sexo.Mujer,FechaNacimiento=DateTime.Parse( "30/10/2001"),CURP="sadfsd",Activo=true,Nombre="Maria Soto" });   
-
-            foreach(Alumno item in alumnos.Where(a=> a.Sexo==Sexo.Mujer && a.Activo==false))
-            {
-                Console.WriteLine($"{item.IdAlumno} {item.Nombre} {item.Sexo} " );
-            } 
-
-       */
-
-
             bool Salir = false;
             while (!Salir)
             {
                 Console.Clear();
-                Console.WriteLine("Teclee una opcion:");
+                Console.WriteLine("Menu");
+                Console.WriteLine("_______________________________");
+                Console.WriteLine("Alta alumnos ................ 1");
+                Console.WriteLine("Borrar lista de alumnos ..... 2");
+                Console.WriteLine("Borrar alumno  .............. 3");
+                Console.WriteLine("Actualizar alumno  .......... 4");
+                Console.WriteLine("Listar alumnos .............. 5");
+                Console.WriteLine("Alta Materias ............... 6");
+                Console.WriteLine("Borrar lista de Materias .... 7");
+                Console.WriteLine("Borrar Materias ............. 8");
+                Console.WriteLine("Actualizar Materia .......... 9");
+                Console.WriteLine("Listar Materias..............10");
+                Console.WriteLine("Salir ....................... 0");
+                Console.WriteLine("_______________________________");
+                Console.Write("Opcion=> ");
                 int Opcion = int.Parse(Console.ReadLine());
                 switch (Opcion)
                 {
@@ -58,7 +35,31 @@ namespace Proyecto_clases
                         GuardarAlumno();
                         break;
                     case 2:
+                        BaseDatos.BorrarListaAlumnos();
+                        break;
+                    case 3:
+                        BorrarAlumno();
+                        break;
+                    case 4:
+                        ActualizarAlumno();
+                        break;
+                    case 5:
                         ListarAlumno();
+                        break;
+                    case 6:
+                        GuardarMateria();
+                        break;
+                    case 7:
+                        BaseDatos.BorrarListaMeterias();
+                        break;
+                    case 8:
+                        BorrarMateria();
+                        break;
+                    case 9:
+                        ActualizarMateria();
+                        break;
+                    case 10:
+                        ListarMateria();
                         break;
                     default:
                         Console.WriteLine("Opcion no valida");
@@ -69,7 +70,9 @@ namespace Proyecto_clases
 
             void GuardarAlumno()
             {
-                Console.Clear();
+                Console.Clear();//limpia pantalla
+                Console.WriteLine("Alta Alumno");
+                Console.WriteLine("____________________________");
                 Alumno alumno = new Alumno();
 
                 Console.WriteLine("Codigo");
@@ -78,22 +81,149 @@ namespace Proyecto_clases
                 Console.WriteLine("Nombre");
                 alumno.Nombre = Console.ReadLine();
 
+                Console.WriteLine("Curp");
+                alumno.Nombre = Console.ReadLine();
+
+
+
                 BaseDatos.Guardar(alumno);
+
+                Console.WriteLine("____________________________");
+
+                Console.WriteLine("Se guardo corectamente...!");
+                Console.ReadKey();
 
             }
             void ListarAlumno()
             {
-                Console.Clear();
+                Console.Clear();//limpia pantalla
+                Console.WriteLine("Lista de Alumnos");
+                Console.WriteLine("____________________________");
 
-                foreach (Alumno item in BaseDatos.Listar())
+                foreach (Alumno item in BaseDatos.ListarAlumno())
                 {
                     Console.WriteLine($"{item.IdAlumno} {item.Nombre}");
                 }
 
 
+                Console.WriteLine("____________________________");
+                Console.WriteLine($"Total de registros : {BaseDatos.ListarAlumno().Count}");
+
+                Console.WriteLine("");
                 Console.WriteLine("Precione una tecla para continuar");
                 Console.ReadKey();
             }
+        }
+        private static void ActualizarAlumno()
+        {
+            Console.Clear();//limpia pantalla
+            Console.WriteLine("Modificar Alumno");
+            Console.WriteLine("____________________________");
+            Alumno alumno = new Alumno();
+
+            Console.WriteLine("Codigo");
+            var Codigo = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Nombre");
+            alumno.Nombre = Console.ReadLine();
+
+            Console.WriteLine("Curp");
+            alumno.CURP = Console.ReadLine();
+
+            BaseDatos.Actualizar(Codigo, alumno);
+
+            Console.WriteLine("____________________________");
+
+            Console.WriteLine("Se Modifico corectamente...!");
+            Console.ReadKey();
+        }
+        private static void BorrarAlumno()
+        {
+            Console.Clear();//limpia pantalla
+            Console.WriteLine("Borrar Alumno");
+            Console.WriteLine("____________________________");
+            Console.WriteLine("Codigo :");
+            var Codigo = int.Parse(Console.ReadLine());
+            BaseDatos.BorrarAlumno(Codigo);
+
+            Console.WriteLine("____________________________");
+
+            Console.WriteLine("Se borro corectamente...!");
+            Console.ReadKey();
+        }
+        private static void ListarMateria()
+        {
+            Console.Clear();//limpia pantalla
+            Console.WriteLine("Lista de Materias");
+            Console.WriteLine("____________________________");
+            foreach (Materia item in BaseDatos.ListarMateria())
+            {
+                Console.WriteLine($"{item.IdMateria} - {item.Nombre}");
+            }
+            Console.WriteLine("____________________________");
+            Console.WriteLine($"Total de registros : {BaseDatos.ListarMateria().Count}");
+            Console.WriteLine("");
+            Console.WriteLine("Precione una tecla para continuar");
+            Console.ReadKey();
+
+        }
+        private static void GuardarMateria()
+        {
+            Console.Clear();//limpia pantalla
+            Console.WriteLine("Alta Materia");
+            Console.WriteLine("____________________________");
+            Materia materia = new Materia();
+
+            Console.WriteLine("Codigo :");
+            materia.IdMateria = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nombre :");
+            materia.Nombre = Console.ReadLine();
+
+            BaseDatos.Guardar(materia);
+            Console.WriteLine("____________________________");
+
+            Console.WriteLine("Se guardo corectamente...!");
+            Console.ReadKey();
+
+        }
+        private static void ActualizarMateria()
+        {
+            Console.Clear();//limpia pantalla
+            Console.WriteLine("Modificar Materia");
+            Console.WriteLine("____________________________");
+            Materia materia = new Materia();
+
+            Console.WriteLine("Codigo");
+            var Codigo = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Nombre");
+            materia.Nombre = Console.ReadLine();
+
+
+
+            BaseDatos.Actualizar(Codigo, materia);
+
+            Console.WriteLine("____________________________");
+
+            Console.WriteLine("Se Modifico corectamente...!");
+            Console.ReadKey();
+        }
+        private static void BorrarMateria()
+        {
+            Console.Clear();//limpia pantalla
+            Console.WriteLine("Borrar materia");
+            Console.WriteLine("____________________________");
+            Console.WriteLine("Codigo :");
+            var Codigo = int.Parse(Console.ReadLine());
+            BaseDatos.BorrarMateria(Codigo);
+
+            Console.WriteLine("____________________________");
+
+            Console.WriteLine("Se borro corectamente...!");
+            Console.ReadKey();
         }
     }
 }
